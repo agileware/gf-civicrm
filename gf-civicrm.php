67327983aba45be6e965562207d2184f0db4865b
Justin Freeman <justin@agileware.com.au>
2023-07-10 10:46:46 +1000

    GFCV-45 Version 1.4.1

--- gf-civicrm.php
+++ gf-civicrm.php
@@ -5,7 +5,7 @@
  * Description: Extends Gravity Forms to get option lists and defaults from linked CiviCRM Form Processors
  * Author: Agileware
  * Author URI: https://agileware.com.au
- * Version: 1.4.0
+ * Version: 1.4.1
  * Text Domain: gf-civicrm
  *
  * Gravity Forms CiviCRM Integration is free software: you can redistribute it and/or modify
