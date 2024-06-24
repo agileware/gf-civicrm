86aeabc4aea5ad720fd24149d3676344b5a1afbf
Iris Abarquez <iris@agileware.com.au>
2024-06-24 15:00:53 +1000

    GFCV-81 Update custom exception class error logging

--- includes/class-gf-civicrm-exception.php
+++ includes/class-gf-civicrm-exception.php
@@ -9,12 +9,28 @@ class GFCiviCRM_Exception extends Exception
         parent::__construct($message, $code, $previous);
     }
 
-    public function getErrorLogMessage( $include_trace = false ): string {
-        $error_log = $this->getMessage();
+    /**
+     * NOTE: 
+     * Invalid Entities and Actions will *sometimes* print out as hashed codes when the cache is on for wpcmrf_api().
+     * This is because failed API calls can be cached without an Entity or Action in the $call.
+     * When creating post data for the cURL request, these are then set to null and hashed.
+     * 
+     * Fixing this will depend on this issue in CMRF: https://github.com/CiviMRF/CMRF_Abstract_Core/issues/24
+     */
+    public function getErrorMessage( $message = '', $include_trace = false ): string {
+        $error_log = $message . " " . $this->getMessage() . " on line " . $this->getLine() . " in " . $this->getFile();
         if ( $include_trace ) {
-            $error_log .= $this->getTraceAsString();
+            $error_log .= "\n" . $this->getTraceAsString();
         }
         return $error_log;
+    }
+
+    public function logErrorMessage( $include_trace = false ) {
+        if ( WP_DEBUG && WP_DEBUG_LOG ) {
+            error_log( print_r( $this->getErrorMessage( $include_trace ), true ) );
+        } else if ( WP_DEBUG && !WP_DEBUG_LOG ) {
+            error_log( print_r( $this->getErrorMessage( $include_trace ) ) );
+        }
     }
 
 }
