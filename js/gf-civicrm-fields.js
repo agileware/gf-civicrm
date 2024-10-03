1ee757c7675f813beb0cfd343ca0184bb2adb76a
Iris Abarquez <iris@agileware.com.au>
2024-10-03 17:05:34 +1000

    GFCV-116 Remove JS that sets Show Values

--- js/gf-civicrm-fields.js
+++ js/gf-civicrm-fields.js
@@ -1,30 +1,5 @@
-if ( typeof gform !== 'undefined' ) {
-    // Default the "Show values" option to true
-    gform.addAction( 'gform_post_set_field_property', function ( name, field, value, previousValue ) {
-        if ( name === 'civicrmOptionGroup' ) {
-            enableFieldChoiceValues();
-        }
-    } );
-
-    // Enable the "Show Values" option if a CiviCRM Source was selected
-    jQuery(document).on('gform_load_field_settings', function( event, field, form ){
-        if ( field['civicrmOptionGroup'] && field['civicrmOptionGroup'] != '' ) {
-            enableFieldChoiceValues();
-        }
-    });
-
-    function enableFieldChoiceValues() {
-        var field_choice_values_enabled = jQuery('#field_choice_values_enabled');
-
-        field_choice_values_enabled.prop( "checked", true );
-        field_choice_values_enabled["enableChoiceValue"] = true;
-
-        ToggleChoiceValue();
-    }
-}
-
 /**
- * Form field input validation.
+ * Form field input validation for the frontend
  */
 
 (function (doc, $) {
