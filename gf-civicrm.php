744f9c6fa4f0d8ddb848641f3b03e9b910bd27e8
Iris Abarquez <iris@agileware.com.au>
2025-02-11 17:03:40 +1100

    Bump version 1.10.3-beta-11

--- gf-civicrm.php
+++ gf-civicrm.php
@@ -6,7 +6,7 @@
  * Requires plugins: civicrm, gravityforms
  * Author: Agileware
  * Author URI: https://agileware.com.au
- * Version: 1.10.3-beta-10
+ * Version: 1.10.3-beta-11
  * Text Domain: gf-civicrm
  * 
  * Copyright (c) Agileware Pty Ltd (email : support@agileware.com.au)
