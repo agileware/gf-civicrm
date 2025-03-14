52fd85dbdffece8244abee24d84bea04b8022379
Iris Abarquez <iris@agileware.com.au>
2025-03-14 16:25:57 +1100

    GFCV-137 Bump beta version 1.10.3-beta-13

--- gf-civicrm.php
+++ gf-civicrm.php
@@ -6,7 +6,7 @@
  * Requires plugins: civicrm, gravityforms
  * Author: Agileware
  * Author URI: https://agileware.com.au
- * Version: 1.10.3-beta-12
+ * Version: 1.10.3-beta-13
  * Text Domain: gf-civicrm
  * 
  * Copyright (c) Agileware Pty Ltd (email : support@agileware.com.au)
