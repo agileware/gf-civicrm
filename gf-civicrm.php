095152b360c70fd9e60aa991f5870236a3eb68a4
Iris Abarquez <iris@agileware.com.au>
2025-09-29 13:35:48 +1000

    GFCV-179 Version 2.0.0-alpha-2

--- gf-civicrm.php
+++ gf-civicrm.php
@@ -6,7 +6,7 @@
  * Requires plugins: gravityforms
  * Author: Agileware
  * Author URI: https://agileware.com.au
- * Version: 2.0.0-alpha-1
+ * Version: 2.0.0-alpha-2
  * Text Domain: gf-civicrm
  * 
  * Copyright (c) Agileware Pty Ltd (email : support@agileware.com.au)
