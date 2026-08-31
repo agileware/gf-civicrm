12e8226b3456b076a1d0643b69d5f83a4af9ba7e
Justin Freeman <justin@agileware.com.au>
2026-08-31 14:20:46 +1000

    Task-2800 - Version 1.12.1

--- gf-civicrm.php
+++ gf-civicrm.php
@@ -6,7 +6,7 @@
  * Requires plugins: civicrm, gravityforms
  * Author: Agileware
  * Author URI: https://agileware.com.au
- * Version: 1.12.0
+ * Version: 1.12.1
  * Text Domain: gf-civicrm
  * 
  * Copyright (c) Agileware Pty Ltd (email : support@agileware.com.au)
